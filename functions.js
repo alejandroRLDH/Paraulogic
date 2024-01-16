74cb46abfe41105b893685452ad8d3515fedaea0
alejandroroman <alejandroroman@alumnat.iesdamiahuguet.net>
2024-01-16 13:04:19 +0100

    Commit 3 Caselles es comproven i pinten depenent de si la paraula hi és, és a la paraula o no hi és.

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-let paraula = RATON
+const paraula = "RATON"
 
 function send(){
     let resposta = document.getElementById('resposta').value;
@@ -10,7 +10,23 @@ function send(){
             let lletra = resposta.charAt(i-1);
             document.getElementById(id).innerHTML = lletra;
         }
+        comprova()
     }else{
     document.getElementById('no5').hidden = false;
+    }
+}
+
+function comprova(){
+    let resposta = document.getElementById('resposta').value;
+    resposta = resposta.toUpperCase();
+    for (i=1;i<resposta.length+1;i++){
+        let id = "lletra" + i;
+        if (resposta.charAt(i-1)==paraula.charAt(i-1)) {
+            document.getElementById(id).className = "slotgreen";
+        }else if(paraula.includes(resposta.charAt(i-1))){
+            document.getElementById(id).className = "slotyellow";
+        }else{
+            document.getElementById(id).className = "slot";
+        }
     }
 }
