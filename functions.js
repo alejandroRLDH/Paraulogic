6670994554441c2f5c1e947ecb121b54a5733d6f
alejandroroman <alejandroroman@alumnat.iesdamiahuguet.net>
2024-01-16 12:28:35 +0100

    Commit 2 Codi de provada de paraula més òptima.

--- functions.js
+++ functions.js
@@ -5,26 +5,12 @@ function send(){
     resposta = resposta.toUpperCase();
     if (resposta.length==5){
         document.getElementById('no5').hidden = true;
-        for (i=0;i<resposta.length;i++){
-            let lletra = resposta.charAt(i);
-            if (i==0){
-                document.getElementById('lletra1').innerHTML = lletra;
-            } else if (i==1){
-                document.getElementById('lletra2').innerHTML = lletra;
-            } else if (i==2){
-                document.getElementById('lletra3').innerHTML = lletra;
-            } else if (i==3){
-                document.getElementById('lletra4').innerHTML = lletra;
-            } else if (i==4){
-                document.getElementById('lletra5').innerHTML = lletra;
-            }
+        for (i=1;i<resposta.length+1;i++){
+            let id = "lletra" + i;
+            let lletra = resposta.charAt(i-1);
+            document.getElementById(id).innerHTML = lletra;
         }
-        comprova();
     }else{
     document.getElementById('no5').hidden = false;
     }
-}
-
-function comprova(){
-    if=
 }
