dfc1a9816ffa90da58135639ce5c1e49f1f65d76
alejandroroman <alejandroroman@alumnat.iesdamiahuguet.net>
2024-01-17 14:05:31 +0100

    Commit 4 Caselles es creen. Es pot guanyar i hi ha un intent de poder perdre.

--- functions.js
+++ functions.js
@@ -1,16 +1,23 @@
 const paraula = "RATON"
+let count = 0
 
 function send(){
     let resposta = document.getElementById('resposta').value;
     resposta = resposta.toUpperCase();
     if (resposta.length==5){
         document.getElementById('no5').hidden = true;
-        for (i=1;i<resposta.length+1;i++){
-            let id = "lletra" + i;
+        count++
+        crear()
+        if(count==6){
+            document.getElementById("boton").hidden = "true";
+            window.alert("Has perdut!")
+        }else{ for (i=1;i<resposta.length+1;i++){
+            let id = "lletra" + count + i;
             let lletra = resposta.charAt(i-1);
             document.getElementById(id).innerHTML = lletra;
+            }
+            comprova()
         }
-        comprova()
     }else{
     document.getElementById('no5').hidden = false;
     }
@@ -20,7 +27,7 @@ function comprova(){
     let resposta = document.getElementById('resposta').value;
     resposta = resposta.toUpperCase();
     for (i=1;i<resposta.length+1;i++){
-        let id = "lletra" + i;
+        let id = "lletra" + count + i;
         if (resposta.charAt(i-1)==paraula.charAt(i-1)) {
             document.getElementById(id).className = "slotgreen";
         }else if(paraula.includes(resposta.charAt(i-1))){
@@ -29,4 +36,18 @@ function comprova(){
             document.getElementById(id).className = "slot";
         }
     }
+    if(resposta==paraula){
+        document.getElementById("boton").hidden = "true";
+        window.alert("Has guanyat!");
+    }
+}
+
+function crear(){
+    let linea = "<div class=\"resposta\">" + '<br>';
+    for (i=1;i<6;i++){
+        let id = "lletra" + count + i;
+        linea += "<div class=\"slot\" id=\"" + id +  "\"></div>" + '<br>';
+    }
+    linea += "</div>" + "<br>"
+    document.getElementById("respostes").innerHTML += linea;
 }
