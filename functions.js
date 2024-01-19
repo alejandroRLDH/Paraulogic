4673ac163eb67c31efa335ab5d30122c3dc34b9b
alejandroroman <alejandroroman@alumnat.iesdamiahuguet.net>
2024-01-19 08:29:17 +0100

    Commit 5 Es pot perdre correctament + canvi de paraula.

--- functions.js
+++ functions.js
@@ -1,22 +1,22 @@
-const paraula = "RATON"
+const paraula = "BAZINGA"
 let count = 0
 
 function send(){
     let resposta = document.getElementById('resposta').value;
     resposta = resposta.toUpperCase();
-    if (resposta.length==5){
+    if (resposta.length==7){
         document.getElementById('no5').hidden = true;
         count++
         crear()
-        if(count==6){
-            document.getElementById("boton").hidden = "true";
-            window.alert("Has perdut!")
-        }else{ for (i=1;i<resposta.length+1;i++){
+        for (i=1;i<resposta.length+1;i++){
             let id = "lletra" + count + i;
             let lletra = resposta.charAt(i-1);
             document.getElementById(id).innerHTML = lletra;
-            }
-            comprova()
+        }
+        comprova()
+        if(count==5){
+            document.getElementById("boton").hidden = "true";
+            window.alert("Has perdut!")
         }
     }else{
     document.getElementById('no5').hidden = false;
@@ -39,12 +39,13 @@ function comprova(){
     if(resposta==paraula){
         document.getElementById("boton").hidden = "true";
         window.alert("Has guanyat!");
+        count--
     }
 }
 
 function crear(){
     let linea = "<div class=\"resposta\">" + '<br>';
-    for (i=1;i<6;i++){
+    for (i=1;i<paraula.length+1;i++){
         let id = "lletra" + count + i;
         linea += "<div class=\"slot\" id=\"" + id +  "\"></div>" + '<br>';
     }
