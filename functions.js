602e74ebab6a1f7c16e180ca639efa810335b747
alejandroroman <alejandroroman@alumnat.iesdamiahuguet.net>
2024-01-19 08:48:30 +0100

    Commit 6 Pistes canviant segons els teus intents + comentaris.

--- functions.js
+++ functions.js
@@ -1,31 +1,39 @@
+// Es determinen la paraula i un comptador d'intents.
 const paraula = "BAZINGA"
 let count = 0
 
 function send(){
+    //Agafa la resposta i es posa en majúscula per poder usarla.
     let resposta = document.getElementById('resposta').value;
     resposta = resposta.toUpperCase();
     if (resposta.length==7){
-        document.getElementById('no5').hidden = true;
-        count++
-        crear()
+        //Es quita l'alerta que diu que no té lletres correctes, i suma un al comptador d'intents.
+        document.getElementById('no').hidden = true;
+        count++;
+        crear();
+        //El for asigna una lletra a cada casella, utilitzant el comptador per cambiar d'ID (L'ID de les caselles son lletra + intent + número).
         for (i=1;i<resposta.length+1;i++){
             let id = "lletra" + count + i;
             let lletra = resposta.charAt(i-1);
             document.getElementById(id).innerHTML = lletra;
         }
         comprova()
+        //Mecanisme de perdre si has arribat al màxim.
         if(count==5){
             document.getElementById("boton").hidden = "true";
-            window.alert("Has perdut!")
+            document.getElementById("pista").innerHTML = "Mala sort. :/";
+            window.alert("Has perdut!");
         }
     }else{
-    document.getElementById('no5').hidden = false;
+    //Alerta al no tenir nombre de lletres correctes.
+    document.getElementById('no').hidden = false;
     }
 }
 
 function comprova(){
     let resposta = document.getElementById('resposta').value;
     resposta = resposta.toUpperCase();
+    //Utilitzant el mateix sistema d'ID per pintar lletres, comprova si la lletra és a la paraula i pinta colors.
     for (i=1;i<resposta.length+1;i++){
         let id = "lletra" + count + i;
         if (resposta.charAt(i-1)==paraula.charAt(i-1)) {
@@ -36,14 +44,21 @@ function comprova(){
             document.getElementById(id).className = "slot";
         }
     }
+    //Fa els intents. Compta enrera dels intents per saber quants et queden.
+    let intents= 5-count;
+    document.getElementById("pista").innerHTML = "Pista: Tens " + intents + " intents.";
+    //Comprovació de si has guanyat.
     if(resposta==paraula){
         document.getElementById("boton").hidden = "true";
+        document.getElementById("pista").innerHTML = "Felicitats! :D";
         window.alert("Has guanyat!");
-        count--
+        //Resta intents per no alertar el sistema de pèrdua.
+        count--;
     }
 }
 
 function crear(){
+    //Crea les caselles amb codi, usant el mateix sistema d'ID per posar-los.
     let linea = "<div class=\"resposta\">" + '<br>';
     for (i=1;i<paraula.length+1;i++){
         let id = "lletra" + count + i;
